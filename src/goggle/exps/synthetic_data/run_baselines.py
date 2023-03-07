f478e86148eeaae539e1a8c3aeed5b3079845615
robsdavis <robsdavis473@gmail.com>
2023-03-07 16:57:23 +0000

    Update notebooks and remove unused code

--- src/goggle/exps/synthetic_data/run_baselines.py
+++ src/goggle/exps/synthetic_data/run_baselines.py
@@ -1,16 +1,16 @@
-import sys
-import json
+# 3rd Party
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
-
-import synthcity.logger as log
-from synthcity.plugins import Plugins
-from synthcity.metrics import eval_statistical
-from synthcity.metrics import eval_performance
-from synthcity.metrics import eval_detection
-from synthcity.plugins.core.dataloader import GenericDataLoader
-
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Synthcity
+from synthcity.metrics import eval_detection, eval_performance, eval_statistical
+from synthcity.plugins import Plugins
+from synthcity.plugins.core.dataloader import GenericDataLoader
 
 generators = Plugins()
 
@@ -26,12 +26,13 @@ def evaluate_baselines(data, seed):
     gmm_detector = eval_detection.SyntheticDetectionGMM()
 
     X_train, X_test = train_test_split(
-        data, random_state=seed+42, test_size=0.33, shuffle=False)
+        data, random_state=seed + 42, test_size=0.33, shuffle=False
+    )
 
     results = {}
 
-    for model in ['nflow', 'ctgan', 'tvae', 'bayesian_network', 'copulagan']:
-        gen = generators.get(model)
+    for model in ["nflow", "ctgan", "tvae", "bayesian_network", "copulagan"]:
+        gen = generators.get(model, device="cpu")
         gen.fit(X_train)
         X_synth = gen.generate(count=X_test.shape[0])
 
@@ -51,17 +52,14 @@ def evaluate_baselines(data, seed):
         mlp_det = mlp_detector.evaluate(X_test_loader, X_synth_loader)
         gmm_det = gmm_detector.evaluate(X_test_loader, X_synth_loader)
         data_qual = quality_evaluator.evaluate(X_test_loader, X_synth_loader)
+        naive_data_qual = {k: v for (k, v) in data_qual.items() if "naive" in k}
 
-        gt_perf = np.mean([xgb_score['gt'],
-                           linear_score['gt'],
-                           mlp_score['gt']])
-        synth_perf = np.mean([xgb_score['syn_ood'],
-                              linear_score['syn_ood'],
-                              mlp_score['syn_ood']])
-        det_score = np.mean([xgb_det['mean'],
-                             gmm_det['mean'],
-                             mlp_det['mean']])
-        qual_score = np.mean(list(data_qual.values()))
+        gt_perf = np.mean([xgb_score["gt"], linear_score["gt"], mlp_score["gt"]])
+        synth_perf = np.mean(
+            [xgb_score["syn_ood"], linear_score["syn_ood"], mlp_score["syn_ood"]]
+        )
+        det_score = np.mean([xgb_det["mean"], gmm_det["mean"], mlp_det["mean"]])
+        qual_score = np.mean(list(naive_data_qual.values()))
 
         results[model] = [gt_perf, synth_perf, det_score, qual_score]
 
@@ -69,16 +67,18 @@ def evaluate_baselines(data, seed):
 
 
 if __name__ == "__main__":
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.compose import ColumnTransformer
 
-    dataset = 'breast'
-    X = pd.read_csv('../data/breast_cancer_2.csv')
+    dataset = "breast"
+    data_path = Path("src/goggle/exps/data/breast_cancer_2.csv").resolve()
+
+    X = pd.read_csv(data_path)
 
     ind = list(range(len(X.columns)))
     ind = [x for x in ind if x != X.columns.get_loc("target")]
     col_list = X.columns[ind]
-    ct = ColumnTransformer([('scaler', StandardScaler(), col_list)], remainder='passthrough')
+    ct = ColumnTransformer(
+        [("scaler", StandardScaler(), col_list)], remainder="passthrough"
+    )
 
     X_ = ct.fit_transform(X)
     X = pd.DataFrame(X_, index=X.index, columns=X.columns)
--- src/goggle/model/GraphDecoder.py
+++ src/goggle/model/GraphDecoder.py
@@ -1,6 +1,6 @@
 # 3rd Party
+from dgl.nn import GraphConv, SAGEConv
 from torch import nn
-from dgl.nn import SAGEConv, GraphConv
 
 # Goggle
 from goggle.model.RGCNConv import RGCNConv
@@ -11,33 +11,49 @@ class GraphDecoderHomo(nn.Module):
         super(GraphDecoderHomo, self).__init__()
         decoder = nn.ModuleList([])
 
-        if decoder_arch == 'gcn':
+        if decoder_arch == "gcn":
             for i in range(decoder_l):
-                if i == decoder_l-1:
+                if i == decoder_l - 1:
                     decoder.append(
-                        GraphConv(decoder_dim, 1, norm='both', weight=True, bias=True))
+                        GraphConv(decoder_dim, 1, norm="both", weight=True, bias=True)
+                    )
                 else:
-                    decoder_dim_ = int(decoder_dim/2)
-                    decoder.append(GraphConv(decoder_dim, decoder_dim_,
-                                             norm='both', weight=True, bias=True, activation=nn.Tanh()))
+                    decoder_dim_ = int(decoder_dim / 2)
+                    decoder.append(
+                        GraphConv(
+                            decoder_dim,
+                            decoder_dim_,
+                            norm="both",
+                            weight=True,
+                            bias=True,
+                            activation=nn.Tanh(),
+                        )
+                    )
                     decoder_dim = decoder_dim_
-        elif decoder_arch == 'sage':
+        elif decoder_arch == "sage":
             for i in range(decoder_l):
-                if i == decoder_l-1:
+                if i == decoder_l - 1:
                     decoder.append(
-                        SAGEConv(decoder_dim, 1, aggregator_type='mean', bias=True))
+                        SAGEConv(decoder_dim, 1, aggregator_type="mean", bias=True)
+                    )
                 else:
-                    decoder_dim_ = int(decoder_dim/2)
-                    decoder.append(SAGEConv(decoder_dim, decoder_dim_,
-                                            aggregator_type='mean', bias=True, activation=nn.Tanh()))
+                    decoder_dim_ = int(decoder_dim / 2)
+                    decoder.append(
+                        SAGEConv(
+                            decoder_dim,
+                            decoder_dim_,
+                            aggregator_type="mean",
+                            bias=True,
+                            activation=nn.Tanh(),
+                        )
+                    )
                     decoder_dim = decoder_dim_
         else:
-            raise Exception('decoder can only be {het|gcn|sage}')
+            raise Exception("decoder can only be {het|gcn|sage}")
 
         self.decoder = nn.Sequential(*decoder)
 
     def forward(self, graph_input, b_size):
-
         b_z, b_adj, b_edge_weight = graph_input
 
         for layer in self.decoder:
@@ -54,20 +70,31 @@ class GraphDecoderHet(nn.Module):
         decoder = nn.ModuleList([])
 
         for i in range(decoder_l):
-            if i == decoder_l-1:
+            if i == decoder_l - 1:
                 decoder.append(
-                    RGCNConv(decoder_dim, 1, num_relations=n_edge_types+1, root_weight=False))
+                    RGCNConv(
+                        decoder_dim,
+                        1,
+                        num_relations=n_edge_types + 1,
+                        root_weight=False,
+                    )
+                )
             else:
-                decoder_dim_ = int(decoder_dim/2)
+                decoder_dim_ = int(decoder_dim / 2)
                 decoder.append(
-                    RGCNConv(decoder_dim, decoder_dim_, num_relations=n_edge_types+1, root_weight=False))
+                    RGCNConv(
+                        decoder_dim,
+                        decoder_dim_,
+                        num_relations=n_edge_types + 1,
+                        root_weight=False,
+                    )
+                )
                 decoder.append(nn.ReLU())
                 decoder_dim = decoder_dim_
 
         self.decoder = nn.Sequential(*decoder)
 
     def forward(self, graph_input, b_size):
-
         b_z, b_edge_index, b_edge_weights, b_edge_types = graph_input
 
         h = b_z
